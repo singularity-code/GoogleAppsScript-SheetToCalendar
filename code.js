43279f333c5161d2bf6f96779b94f584f00e4340
Chris <neoxrock@gmail.com>
2019-08-19 14:53:00 +1000

    fix

--- code.js
+++ code.js
@@ -3,41 +3,42 @@ function create_event() {
   //var activeSheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();  // To test with active sheet
   var sheet = SpreadsheetApp.openById("").getActiveSheet(); // Place your sheet ID connected with Form
   var formId = ''; // Place your Form ID
+
   var data = get_sheetData();
+
+  // This sheet ranges are depends on your sheet structure.
   var names = sheet.getRange("B2:B").getValues();
-  var eventIds = sheet.getRange("R2:R").getValues();
-  var status = sheet.getRange("Q2:Q").getValues();
+  var status = sheet.getRange("T2:T").getValues();
+  var eventIds = sheet.getRange("N2:N").getValues();
+  var eventResult = sheet.getRange("U2:U").getValues();
   var targets = [];
     
-  //Logger.log(data);
   if(data) {
     for(var k = 0; k < eventIds.length; k++) {
-      if(eventIds[k].toString() != 'Y' && status[k] == 'Complete') {
+      if(eventResult[k].toString() != 'Y' && status[k] == 'Complete') {
         targets.push(k);
       }
     }
  
-    Logger.log(targets);
     for(var j = 0; j < targets.length; j++) {
       var result;
       var index = targets[j];
       result = calendar.createEvent(data[index].name, new Date(data[index].startTime), new Date(data[index].endTime), data[index].description);
-      sheet.getRange(data[index].rowNo, 11).setValue(result.getId());
-      sheet.getRange(data[index].rowNo, 18).setValue("Y");
+      if(result) {
+        sheet.getRange(data[index].rowNo, 14).setValue(result.getId());
+        sheet.getRange(data[index].rowNo, 21).setValue("Y");
+      }
     }
   }
 }
+
 function get_sheetData() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  var range = sheet.getRange("Q1:Q");
+  var sheet = SpreadsheetApp.openById("1LkVbEzPjHWXB-_LY2INGudMMTLkJXnhsjRlfPm6cldc").getActiveSheet();
+  var range = sheet.getRange("T1:T");
   var value = range.getValues();
   var result = [];
-  //Logger.log(value)
-  //Logger.log(value.length);
   for(var i = 1; i < value.length; i++) {
     if(value[i] != '') {
-      //Logger.log(value[i].toString());
-      //Logger.log(sheet.getRange(i + 1, 1, 1, 17).getValues());
       var resultRow = sheet.getRange(i + 1, 1, 1, 17).getValues();
       var obj = {};
       obj.name = resultRow[0][1].toString() + " " + resultRow[0][4].toString();
